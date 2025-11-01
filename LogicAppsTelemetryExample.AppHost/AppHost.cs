260f22754a9db848b5eeb410713076757350ef1b
Tom Wolverson <94133561+TWolversonReply@users.noreply.github.com>
2025-11-01 15:33:48 +0000

    Add a service bus connection and a message handler and rename the message publisher

--- LogicAppsTelemetryExample.AppHost/AppHost.cs
+++ LogicAppsTelemetryExample.AppHost/AppHost.cs
@@ -4,7 +4,7 @@ using Aspire.Hosting;
 var builder = DistributedApplication.CreateBuilder(args);
 
 var storage = builder.AddAzureStorage("storage")
-    .RunAsEmulator( azurite =>
+    .RunAsEmulator(azurite =>
                      {
                          azurite.WithBlobPort(10000)
                                 .WithQueuePort(10001)
@@ -15,11 +15,18 @@ var blobs = storage.AddBlobContainer("blobs");
 var queue = storage.AddQueues("queue");
 var table = storage.AddTables("table");
 
+var serviceBus = builder.AddAzureServiceBus("servicebus")
+    .RunAsEmulator();
+
+serviceBus.AddQueue("esb");
+
 var logicapps = builder.AddExecutable("logicapps", Environment.ExpandEnvironmentVariables(@"%userprofile%\.azurelogicapps\dependencies\FuncCoreTools\func.exe"), "..\\LogicAppsTelemetryExample", args: ["host", "start", "--runtime", "inproc8", "--language-worker", "dotnet"])
     .WithEnvironment("PATH", $"%PATH%;{Environment.ExpandEnvironmentVariables(@"%userprofile%\.azurelogicapps\dependencies\NodeJs\")};{Environment.ExpandEnvironmentVariables(@"%userprofile%\.azurelogicapps\dependencies\DotNetSDK\")}")
     .WithEnvironment("OTEL_EXPORTER_OTLP_ENDPOINT", "https://localhost:21265")
     .WithEnvironment("OTEL_SERVICE_NAME", "LogicAppsStandardExample")
     .WithEnvironment("OTEL_EXPORTER_OTLP_HEADERS", $"x-otlp-api-key={builder.Configuration["AppHost:OtlpApiKey"]}")
-    .WaitFor(blobs);
+    .WaitFor(blobs)
+    .WithEnvironment("serviceBus_connectionString", serviceBus)
+    .WaitFor(serviceBus);
 
 builder.Build().Run();
